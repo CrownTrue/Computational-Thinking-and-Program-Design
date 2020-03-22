be81babfd8b0321d379ef8cced7716adde4920c2
CrownTrue <61856900+CrownTrue@users.noreply.github.com>
2020-03-22 20:07:24 +0800

    Add files via upload

---
+++ week3.py
@@ -0,0 +1,8 @@
+#儲存成物件 字串要加""
+movie_title="Avengers:Endgame"
+movie_rating=8.4
+movie_time=181
+#使用 type() 函數將這三個物件的類別印出來
+print(type(movie_title))
+print(type(movie_rating))
+print(type(movie_time))
